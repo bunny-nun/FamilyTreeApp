bb6bd15c3bc1b1b79b993a6b74be21e7028021a7
Valeria <nelera.gb@gmail.com>
2023-09-23 13:11:25 +0300

    Class App created

---
+++ src/main/java/ui/App.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Family Tree App");
+        stage.getIcons().add(new Image("D:\\PROJECTS\\FamilyTreeApp\\src\\main\\resources\\ui\\tree.png"));
+        showMainMenu(stage);
+    }
+
+    public void showMainMenu(Stage stage) throws IOException {
+        stage.setResizable(false);
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("start.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,30 +1,7 @@
 package ui;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        stage.setTitle("Family Tree App");
-        stage.getIcons().add(new Image("D:\\PROJECTS\\FamilyTreeApp\\src\\main\\resources\\ui\\tree.png"));
-        showMainMenu(stage);
-    }
-
-    public void showMainMenu(Stage stage) throws IOException {
-        stage.setResizable(false);
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("start.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setScene(scene);
-        stage.show();
+        App.main(args);
     }
 }
